e6df38eca4f6f5053ee987eedd97c96bfe046528
Sand004 <84405739+Sand004@users.noreply.github.com>
2025-06-04 09:53:31 +0200

    Add DOCXProcessor to ingestion module exports

--- src/ingestion/__init__.py
+++ src/ingestion/__init__.py
@@ -2,12 +2,14 @@
 
 from .base import BaseDocumentProcessor
 from .pdf_processor import PDFProcessor
+from .docx_processor import DOCXProcessor
 from .chunking import SemanticChunker
 from .metadata import MetadataExtractor
 
 __all__ = [
     "BaseDocumentProcessor",
     "PDFProcessor",
+    "DOCXProcessor",
     "SemanticChunker",
     "MetadataExtractor",
 ]
