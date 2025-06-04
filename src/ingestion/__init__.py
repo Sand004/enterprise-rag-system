899b39e46b3fff9a20325f4d41425b426d5b5e7f
Sand004 <84405739+Sand004@users.noreply.github.com>
2025-06-04 09:30:45 +0200

    Remove PowerPoint processor from ingestion module

--- src/ingestion/__init__.py
+++ src/ingestion/__init__.py
@@ -2,14 +2,12 @@
 
 from .base import BaseDocumentProcessor
 from .pdf_processor import PDFProcessor
-from .pptx_processor import PowerPointProcessor
 from .chunking import SemanticChunker
 from .metadata import MetadataExtractor
 
 __all__ = [
     "BaseDocumentProcessor",
     "PDFProcessor",
-    "PowerPointProcessor",
     "SemanticChunker",
     "MetadataExtractor",
 ]
